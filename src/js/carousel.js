f446264174fe9da210820bb68a77d48d84a9180b
Ganesh Murali <ganesh.murali@anywhere.co>
2023-07-24 11:54:39 +0530

    Added Js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,26 +1,26 @@
 
-// const { Swiper } = require('swiper');
-// const { Navigation } = require('swiper/modules');
+const { Swiper } = require('swiper');
+const { Navigation } = require('swiper/modules');
 
-// //  Initialize Swiper
-// let swiper = new Swiper(".swiper", {
-//     modules: [Navigation],
-//     loop: true,
-//     grabCursor: true,
-//     speed: 1000,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1,
-//         },
-//         520: {
-//             slidesPerView: 3,
-//         },
-//         950: {
-//             slidesPerView: 4,
-//         },
-//     },
-// });
+//  Initialize Swiper
+let swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    loop: true,
+    grabCursor: true,
+    speed: 1000,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 3,
+        },
+        950: {
+            slidesPerView: 4,
+        },
+    },
+});
--- src/js/loadGraph.js
+++ src/js/loadGraph.js
@@ -1,23 +1,23 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const coinsWrapper = document.getElementById('gph_rht_coins');
-//   const chartList = document.querySelectorAll('.chart');
+document.addEventListener("DOMContentLoaded", () => {
+  const coinsWrapper = document.getElementById('gph_rht_coins');
+  const chartList = document.querySelectorAll('.chart');
 
-//   const getCoinId = (id) => {
-//     console.log('clicked =', id)
-//     chartList.forEach((chart) => {
-//       if (chart.getAttribute('data-content') == id) {
-//         chart.setAttribute("class", "candle_chart chart show");
-//       } else {
-//         chart.setAttribute("class", "candle_chart chart hide");
-//       }
-//     })
-//   }
+  const getCoinId = (id) => {
+    console.log('clicked =', id)
+    chartList.forEach((chart) => {
+      if (chart.getAttribute('data-content') == id) {
+        chart.setAttribute("class", "candle_chart chart show");
+      } else {
+        chart.setAttribute("class", "candle_chart chart hide");
+      }
+    })
+  }
 
-//   coinsWrapper.addEventListener('click', (e) => {
-//     const item = e.target.closest('.gph_rht_coins_itm');
-//     if (item != null) {
-//       const id = item.dataset.id;
-//       getCoinId(id);
-//     }
-//   })
-// });
+  coinsWrapper.addEventListener('click', (e) => {
+    const item = e.target.closest('.gph_rht_coins_itm');
+    if (item != null) {
+      const id = item.dataset.id;
+      getCoinId(id);
+    }
+  })
+});
